5ebccbd82f45bd50d3f2528f4af86d4c6e19c9b3
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-10-28 17:35:05 +0530

    Update PHP_Custom_Snips_Fixes.php

--- PHP_Custom_Snips_Fixes.php
+++ PHP_Custom_Snips_Fixes.php
@@ -1,5 +1,6 @@
 <?php
 # Fix the encoding issues encountered during the use of abroad's languages start
+## $value holds any value either from database or string of special-char language
 $enc = mb_detect_encoding($value, "UTF-8,ISO-8859-1");
 echo ($enc != "UTF-8") ? iconv($enc, "UTF-8", $value) : $value;
 # Fix the encoding issues encountered during the use of abroad's languages finish....
