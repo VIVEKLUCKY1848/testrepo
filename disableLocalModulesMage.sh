1dfbdb3b6e37e4a99f99120a4ac364a26c53bf1c
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-11-11 00:32:00 +0530

    Update disableLocalModulesMage.sh

--- disableLocalModulesMage.sh
+++ disableLocalModulesMage.sh
@@ -4,3 +4,6 @@ for filename in *.xml; do grep "<codePool>local" $filename;
 		sed -i.bak 's/true/false/g' "$filename"
 	fi
 done
+
+## One liner of above block ##
+for filename in *.xml; do grep "<codePool>local" $filename; if [[ $filename != *"Mage_"* ]]; then sed -i.bak 's/true/false/g' "$filename"; fi; done
