ba1cb65a808009acb2abfaacfd2b2bbef846136d
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-11-11 00:58:05 +0530

    Update disableCommunityModulesMage.sh

--- disableCommunityModulesMage.sh
+++ disableCommunityModulesMage.sh
@@ -1,6 +1,8 @@
-for filename in *.xml; do grep "<codePool>community" $filename;
-	if [[ $filename != *"Mage_"* ]]; then
-		# echo "$filename";
-		sed -i.bak 's/true/false/g' "$filename"
+for filename in *.xml; do
+	if grep -q '<codePool>community</codePool>' "$filename"; then
+		if [[ $filename != *"Mage_"* ]]; then
+			# echo "$filename";
+			sed -i.bak 's/true/false/g' "$filename"
+		fi
 	fi
 done
