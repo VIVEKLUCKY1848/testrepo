edaa0a51ddcb7ca8309e706c197d021dde3d9ece
VS <VIVEKLUCKY249@users.noreply.github.com>
2016-11-28 11:58:09 +0530

    Update Linux_shell_snippets.sh

--- Linux_shell_snippets.sh
+++ Linux_shell_snippets.sh
@@ -1,3 +1,7 @@
 #Remove all multiple blank lines and keep only single newline in all the files of directory recursively start
 find . -type f -exec sed -i '/^$/d' '{}' ';'
 #Remove all multiple blank lines and keep only single newline in all the files of directory recursively end...
+
+#Basic command to delete bash/terminal/command-line history start
+history -cw
+#Basic command to delete bash/terminal/command-line history end...
