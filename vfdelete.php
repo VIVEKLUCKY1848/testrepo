b23c2157e4017198321f17a0d9abb71109fa2bab
VS <VIVEKLUCKY249@users.noreply.github.com>
2016-11-29 20:55:39 +0530

    Update vfdelete.php

--- vfdelete.php
+++ vfdelete.php
@@ -1,71 +1,54 @@
 <?php
-
-//$dbhost = 'localhost';
-  // $dbuser = 'root';
-   //$dbpass = 'ps123';
-   //$dbName = 'magento_catalytic_trial';
-
-
-define('MAGENTO_ROOT', getcwd()); 
-$mageFilename = MAGENTO_ROOT . '/app/Mage.php'; 
-require_once $mageFilename; Mage::setIsDeveloperMode(true); 
+define('MAGENTO_ROOT', getcwd());
+$mageFilename = MAGENTO_ROOT . '/app/Mage.php';
+require_once $mageFilename; Mage::setIsDeveloperMode(true);
 $base_url = Mage::getBaseUrl();
+Mage::app('default');
 
-    Mage::app('default');
-    try{
-        $default_setup  = Mage::getConfig()->getResourceConnectionConfig("default_setup");
-        $dbhost = $default_setup->host;
-        $dbuser = $default_setup->username;;
-		$dbpass = $default_setup->password;
-		$dbName = $default_setup->dbname;
-		$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbName);
-    }catch(Exception $e){
-        echo $e;
-    }
+try {
+	$default_setup = Mage::getConfig()->getResourceConnectionConfig("default_setup");
+	$dbhost = $default_setup->host;
+	$dbuser = $default_setup->username;;
+	$dbpass = $default_setup->password;
+	$dbName = $default_setup->dbname;
+	$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbName);
+} catch(Exception $e) {
+	echo $e;
+}
 
-
-
-if ($connection->connect_error) 
-{
+if($connection->connect_error) {
    echo "Error Occurred While Connection To DataBase";
 }
 
-
 //SQL statements separated by semi-colons
-
 $sqlStatements = "SET FOREIGN_KEY_CHECKS = 0;
-TRUNCATE `perception_1_mapping`;
-TRUNCATE `perception_level_1_cat`;
-TRUNCATE `perception_level_1_litre`;
-TRUNCATE `perception_level_1_make`;
-TRUNCATE `perception_level_1_year`;
-TRUNCATE `perception_level_1_model`;
-TRUNCATE `perception_mapping_notes`;
-TRUNCATE `perception_note`;
-TRUNCATE `perception_mapping_paint`;
-ALTER TABLE `perception_1_mapping` AUTO_INCREMENT=1; 
-ALTER TABLE `perception_level_1_cat` AUTO_INCREMENT=1; 
-ALTER TABLE `perception_level_1_litre` AUTO_INCREMENT=1; 
-ALTER TABLE `perception_level_1_make` AUTO_INCREMENT=1; 
-ALTER TABLE `perception_level_1_model` AUTO_INCREMENT=1; 
-ALTER TABLE `perception_level_1_year` AUTO_INCREMENT=1; 
-ALTER TABLE `perception_mapping_notes` AUTO_INCREMENT=1; 
-ALTER TABLE `perception_note` AUTO_INCREMENT=1;
-ALTER TABLE `perception_mapping_paint` AUTO_INCREMENT=1;
+TRUNCATE `company_1_mapping`;
+TRUNCATE `company_level_1_cat`;
+TRUNCATE `company_level_1_litre`;
+TRUNCATE `company_level_1_make`;
+TRUNCATE `company_level_1_year`;
+TRUNCATE `company_level_1_model`;
+TRUNCATE `company_mapping_notes`;
+TRUNCATE `company_note`;
+TRUNCATE `company_mapping_paint`;
+ALTER TABLE `company_1_mapping` AUTO_INCREMENT=1;
+ALTER TABLE `company_level_1_cat` AUTO_INCREMENT=1;
+ALTER TABLE `company_level_1_litre` AUTO_INCREMENT=1;
+ALTER TABLE `company_level_1_make` AUTO_INCREMENT=1;
+ALTER TABLE `company_level_1_model` AUTO_INCREMENT=1;
+ALTER TABLE `company_level_1_year` AUTO_INCREMENT=1;
+ALTER TABLE `company_mapping_notes` AUTO_INCREMENT=1;
+ALTER TABLE `company_note` AUTO_INCREMENT=1;
+ALTER TABLE `company_mapping_paint` AUTO_INCREMENT=1;
 -- TRUNCATE `catalog_compare_item`;
 SET FOREIGN_KEY_CHECKS = 1;";
- 
+
 $sqlResult = $connection->multi_query($sqlStatements);
- 
-if($sqlResult == true) 
-{
-	
+if($sqlResult == true) {
 	Mage::app()->cleanCache();
-	
-   echo 'Successfully clear all vehiclefit data.';
-   header( "refresh:2;url=$base_url" );
-} 
-else 
-{
+	echo 'Successfully clear all vehiclefit data.';
+	header( "refresh:2;url=$base_url" );
+}
+else {
    echo 'Error occurred executing statements.';
 }
